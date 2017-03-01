05ff9a73b4bc82027b90bd71d997587a370b0c41
N Zanghi <N Zanghi>
2017-03-01 00:09:57 -0500

    Hooked up first two pages

--- public/app.module.js
+++ public/app.module.js
@@ -1,10 +1,24 @@
 
 angular
-    .module('burnIt', [
-        'burnIt.main'
+    .module("burnIt", [
+        "burnIt.main",
+        "burnIt.planning"
     ])
-    .controller('burnCtrl', ['$scope', burnCtrl]);
+    .service("CommonSvc", function () {
+        var self = this;
+        var person;
+        self.setPerson = setPerson;
+        self.getPerson = getPerson;
+
+        function setPerson(obj) {
+            person = obj;
+        }
+
+        function getPerson() {
+            return person;
+        }
+    })
+    .controller("burnCtrl", ["$scope", burnCtrl]);
 
 function burnCtrl($scope) {
-    console.log("WORKS!");
 };
--- public/main/main.module.js
+++ public/main/main.module.js
@@ -1,10 +1,10 @@
 (function () {
-    'use strict';
-    angular.module('burnIt.main', ['ngRoute'])
-        .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/', {
-                templateUrl: 'main/views/main.html',
-                controller: 'mainCtrl',
+    "use strict";
+    angular.module("burnIt.main", ["ngRoute"])
+        .config(["$routeProvider", function ($routeProvider) {
+            $routeProvider.when("/", {
+                templateUrl: "main/views/main.html",
+                controller: "MainCtrl",
                 controllerAs: "vm"
             });
         }])
